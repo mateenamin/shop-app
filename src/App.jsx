eeba0ba3fccdf28339f54380c7e2e9080b561c2c
Mateen Amin <mateenamin1122@gmail.com>
2026-06-06 21:36:29 +0500

    Day 1 - project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import ProductDetail from "./Pages/ProductDetail"
+import Cart from "./Pages/Cart"
+import Login from "./Pages/Login"
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-10">
+        Shop
+         App Working!
+      </h1>
+
+      <BrowserRouter>
+      <Routes>
+   <Route path="/" element ={<Home/>}  />
+   <Route path="/products/:id" element ={<ProductDetail/>}  />
+   <Route path="/cart" element ={<Cart/>}  />
+   <Route path="/login" element ={<Login/>}  />
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
